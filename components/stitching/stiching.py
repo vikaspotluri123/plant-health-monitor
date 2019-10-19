f380aa57d4aaf5f727e88220e6dbb4326f724f13
clarksnider <33442923+clarksnider@users.noreply.github.com>
2019-10-18 19:28:39 -0500

    Update stiching.py
    fixed asymmetry

--- components/stitching/stiching.py
+++ components/stitching/stiching.py
@@ -2,60 +2,51 @@ import cv2
 import numpy as np
 
 ## read images using opencv library
-## img_left/right used for disambiguity
-img_left = cv2.imread("field_right.jpg")
-img_right = cv2.imread("field_left.jpg")
+## img_right/left used for disambiguity
+img_right = cv2.imread("field_right.jpg")
+img_left = cv2.imread("field_left.jpg")
 
 ## start sifting
 sift = cv2.xfeatures2d.SIFT_create()
 
 ## find key points
-key_left , des_left = sift.detectAndCompute(img_left, None)
-key_right, des_right = sift.detectAndCompute(img_right, None)
-
-## shows user key points in images
-## cv2.imshow("hawaii_left_keypoints.jpeg",cv2.drawKeypoints(img_left,key_left,None))
-## cv2.imshow("hawaii_right_keypoints.jpeg",cv2.drawKeypoints(img_right,key_right,None))
-## cv2.waitKey(0)
+key_right , des_right = sift.detectAndCompute(img_right, None)
+key_left, des_left = sift.detectAndCompute(img_left, None)
 
 ## method for finding matching key points (to be used to identify overlapping region)
 match = cv2.BFMatcher()
-matches = match.knnMatch(des_left, des_right, k=2)
+matches = match.knnMatch(des_right, des_left, k=2)
 good = []
 for m,n in matches:
-    if m.distance < 0.01*n.distance: ## 0.2 may need to be increased ##
+    if m.distance < 0.1*n.distance: ## 0.2 may need to be increased ##
         good.append(m)
 
 # parameters of lines
 draw_params = dict(matchColor=(0,255,0), singlePointColor=None, flags=2)
 
 ## draw matches
-img_final = cv2.drawMatches(img_left, key_left, img_right, key_right, good, None, **draw_params)
-
-## display matches
-## cv2.imshow("hawaii_final.jpeg", img_final)
-## cv2.waitKey(0)
+## img_matches = cv2.drawMatches(img_right, key_right, img_left, key_left, good, None, **draw_params)
 
 ## find overlapping region using matches
 MIN_MATCH_COUNT = 10
 if len(good) > MIN_MATCH_COUNT:
 
-    src_pts = np.float32([key_left[ m.queryIdx].pt for m in good]).reshape(-1,1,2)
-    dst_pts = np.float32([key_right[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+    src_pts = np.float32([key_right[ m.queryIdx].pt for m in good]).reshape(-1,1,2)
+    dst_pts = np.float32([key_left[m.trainIdx].pt for m in good]).reshape(-1,1,2)
 
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-    h, w, c = img_left.shape
+    h, w, c = img_right.shape
     pts = np.float32([ [0,0] , [0,h-1] , [w-1,h-1] , [w-1,0] ]).reshape(-1,1,2)
     dst = cv2.perspectiveTransform(pts, M)
 
     # shows user the overlapping region with a rectangle
-    # img_right = cv2.polylines(img_right, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+    # img_left = cv2.polylines(img_left, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
 else:
     print("NOT ENOUGH MATCHES AVAILABLE! -", (len(good)/MIN_MATCH_COUNT))
 
-dst = cv2.warpPerspective(img_left, M, (img_right.shape[1] + img_left.shape[1], img_right.shape[0]))
-dst[0:img_right.shape[0], 0:img_right.shape[1]] = img_right
+dst = cv2.warpPerspective(img_right, M, (img_left.shape[1] + img_right.shape[1], img_left.shape[0]))
+dst[0:img_left.shape[0], 0:img_left.shape[1]] = img_left
 
 def trim(frame):
     # crop top
