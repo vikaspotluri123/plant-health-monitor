d4c1a38761d5fb8574c427240cd4b7db441bf794
clarksnider <33442923+clarksnider@users.noreply.github.com>
2019-10-19 15:57:41 -0500

    Update stiching.py

--- components/stitching/stiching.py
+++ components/stitching/stiching.py
@@ -1,5 +1,13 @@
+import os
+os.environ['OPENCV_IO_MAX_IMAGE_PIXELS']=str(2**64)
 import cv2
 import numpy as np
+
+## path to img folder to extract each image
+path = r"C:\Users\clark\School\Ecen 403\Image Processing\venv\Scripts\img_list"
+image_list = []
+for indx, f in enumerate(os.listdir(path), start=1):
+    image_list.append(cv2.imread(os.path.join('img_list', f)))
 
 ## read images using opencv library
 ## img_right/left used for disambiguity
@@ -31,11 +39,15 @@ draw_params = dict(matchColor=(0,255,0), singlePointColor=None, flags=2)
 MIN_MATCH_COUNT = 10
 if len(good) > MIN_MATCH_COUNT:
 
+    # query based off right key
     src_pts = np.float32([key_right[ m.queryIdx].pt for m in good]).reshape(-1,1,2)
+    # train based off of left key
     dst_pts = np.float32([key_left[m.trainIdx].pt for m in good]).reshape(-1,1,2)
 
+    # find similarities between right and left (src and dst)
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
+    # give the left image the pixels from the right image (src -> dst)
     h, w, c = img_right.shape
     pts = np.float32([ [0,0] , [0,h-1] , [w-1,h-1] , [w-1,0] ]).reshape(-1,1,2)
     dst = cv2.perspectiveTransform(pts, M)
@@ -45,27 +57,34 @@ if len(good) > MIN_MATCH_COUNT:
 else:
     print("NOT ENOUGH MATCHES AVAILABLE! -", (len(good)/MIN_MATCH_COUNT))
 
-dst = cv2.warpPerspective(img_right, M, (img_left.shape[1] + img_right.shape[1], img_left.shape[0]))
-dst[0:img_left.shape[0], 0:img_left.shape[1]] = img_left
+## try complete when MIN_MATCH_COUNT was met in above conditional
+try:
+    dst = cv2.warpPerspective(img_right, M, (img_left.shape[1] + img_right.shape[1], img_left.shape[0]))
+    dst[0:img_left.shape[0], 0:img_left.shape[1]] = img_left
 
-def trim(frame):
-    # crop top
-    if not np.sum(frame[0]):
-        return trim(frame[1:])
-    # crop left
-    if not np.sum(frame[-1]):
-        return trim(frame[:-2])
-    # crop right
-    if not np.sum(frame[:,0]):
-        return trim(frame[:,1:])
-    # crop bottom
-    if not np.sum(frame[:,-1]):
-        return trim(frame[:,:-2])
-    return frame
+    # function to erase black regions (caused by residue of src image)
+    def trim(frame):
+        # crop top
+        if not np.sum(frame[0]):
+            return trim(frame[1:])
+        # crop left
+        if not np.sum(frame[-1]):
+            return trim(frame[:-2])
+        # crop right
+        if not np.sum(frame[:,0]):
+            return trim(frame[:,1:])
+        # crop bottom
+        if not np.sum(frame[:,-1]):
+            return trim(frame[:,:-2])
+        return frame
 
-cv2.imshow("hawaii_test_.jpeg", trim(dst))
+    trimmed =  trim(dst)
+    cv2.imshow("hawaii_test_.jpeg", trimmed)
 
-## cv2.imsave("hawaii_test_.jpeg", trim(dst))
-cv2.waitKey(0)
+    ## command to save the composite
+    ## cv2.imsave("hawaii_test_.jpeg", trimmed)
+    cv2.waitKey(0)
 
+except:
+    print("IMAGE STITCH FAILED: NOT ENOUGH MATCHES WERE FOUND")
 ## idea: given more than 2 images, save each composite as the image to be stiched in the next iteration
