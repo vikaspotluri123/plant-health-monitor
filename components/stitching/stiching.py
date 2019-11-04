57c165f853021448866fe31c3e15f33e113681e1
clarksnider <33442923+clarksnider@users.noreply.github.com>
2019-11-04 16:42:00 -0600

    Update stiching.py

--- components/stitching/stiching.py
+++ components/stitching/stiching.py
@@ -3,6 +3,9 @@ os.environ['OPENCV_IO_MAX_IMAGE_PIXELS']=str(2**64)
 import cv2
 import numpy as np
 
+trimmed = []
+final = []
+status = 0
 ## path to img folder to extract each image
 path = r"C:\Users\clark\School\Ecen 403\Image Processing\venv\Scripts\input"
 for indx, f in enumerate(os.listdir(path), start=0):
@@ -26,7 +29,7 @@ for indx, f in enumerate(os.listdir(path), start=0):
         matches = match.knnMatch(des_right, des_left, k=2)
         good = []
         for m,n in matches:
-            if m.distance < n.distance: ## 0.2 may need to be increased ##
+            if m.distance < 0.2 * n.distance: ## 0.2 may need to be increased ##
                 good.append(m)
 
         # parameters of lines
@@ -34,7 +37,10 @@ for indx, f in enumerate(os.listdir(path), start=0):
 
         ## draw matches
         img_matches = cv2.drawMatches(img_right, key_right, img_left, key_left, good, None, **draw_params)
-        #cv2.imshow("img_matches .jpeg", img_matches)
+        # cv2.namedWindow('img_matches', cv2.WINDOW_NORMAL)
+        # cv2.imshow("img_matches", img_matches)
+        # cv2.resizeWindow('img_matches', 1000, 500)
+        # cv2.waitKey(0)
 
         ## find overlapping region using matches
         MIN_MATCH_COUNT = 10
@@ -70,7 +76,7 @@ for indx, f in enumerate(os.listdir(path), start=0):
             if not np.sum(frame[0]):
                 return trim(frame[1:])
             # crop left
-            if not np.sum(frame[-1]):
+            if not np.sum( frame[-1]):
                 return trim(frame[:-2])
             # crop right
             if not np.sum(frame[:,0]):
@@ -81,22 +87,34 @@ for indx, f in enumerate(os.listdir(path), start=0):
             return frame
 
         trimmed =  trim(dst)
-        cv2.imshow("final_img.jpeg", trimmed)
+        cv2.imshow("trimmed.jpeg", trimmed)
         print("write to " + os.listdir(path)[indx+1])
         ## command to save the composite
         cv2.imwrite(os.path.join('input', os.listdir(path)[indx+1]), trimmed)
-
+        status = 1
     except IndexError:
         print("OKAY - RAN OUT OF IMAGES IN INPUT FILE")
-        final = cv2.bilateralFilter(trimmed,9,75,75)
-        cv2.imwrite(path, final)
-        cv2.waitKey(0)
     except OSError:
         print("ERROR - CORRECT FILE NOT FOUND ON MACHINE")
+        status = 0
     except ValueError:
         print("ERROR - NOT ENOUGH MATCHES FOUND - "+len(good)/MIN_MATCH_COUNT)
+        status = 0
     except TabError:
         print("ERROR - TYPE ERROR")
+        status = 0
     except:
         print("ERROR!")
-## idea: given more than 2 images, save each composite as the image to be stiched in the next iteration
+        status = 0
+
+## blur final image if out of images in file
+## Get this to work: currently doesn't write in the right location
+if status == 1:
+    final = cv2.bilateralFilter(trimmed,9,0,75)
+    cv2.namedWindow('final', cv2.WINDOW_NORMAL)
+    cv2.imshow("final", final)
+    cv2.resizeWindow('final', 1000, 500)
+    cv2.imwrite(path+"final.jpg", final)
+    cv2.waitKey(0)
+else:
+    print(Error)
