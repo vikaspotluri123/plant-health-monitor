ccc9e286066fdde55ea7784bc78544698ff76120
Josh Costa <joshcos97@gmail.com>
2020-01-26 18:31:22 -0600

    make analysis able to take input directory

--- components/analysis/main.py
+++ components/analysis/main.py
@@ -1,9 +1,17 @@
 import calibrate
 import index
+import sys
 
-directory = ""
-testing_calibration = calibrate.Calibration()
-testing_calibration.calibrate_prep(directory)
+def analysis_main(directory):
+    testing_calibration = calibrate.Calibration()
+    testing_calibration.calibrate_prep(directory)
 
-testing_index = index.CalculateIndex()
-testing_index.applyIndexing(directory)
+    testing_index = index.CalculateIndex()
+    testing_index.applyIndexing(directory)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: stitching.py inputDirectory")
+        exit()
+
+    analysis_main(sys.argv[1])
