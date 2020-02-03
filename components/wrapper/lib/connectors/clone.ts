9fb07fe79830ddd8b28de12bf39b60c8b578fe7f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-02-03 15:03:07 -0600

    implement clone logic

--- components/wrapper/lib/connectors/clone.ts
+++ components/wrapper/lib/connectors/clone.ts
@@ -1,16 +1,56 @@
 import assert from 'assert';
+import {promises as fs, Stats, PathLike} from 'fs';
+import path from 'path';
+import {EXEC_ERROR} from './base';
 
 const DRIVE_LETTERS = ['d', 'e', 'f', 'g'];
 
-const delay = (time: number) => new Promise(resolve => setTimeout(resolve, time));
+async function getAllFiles(source: string): Promise<string[]> {
+    const fileList: string[] = [];
+
+    for (const file of await fs.readdir(source)) {
+        const fullPath = path.resolve(source, file);
+        const stats = await fs.stat(fullPath);
+
+        if (stats.isDirectory()) {
+            fileList.push(...await getAllFiles(fullPath))
+        } else {
+            fileList.push(fullPath);
+        }
+    }
+
+    return fileList;
+}
 
 export default class AnalysisConnector {
     async exec(drive: string, outputFolder: string) {
+        outputFolder = outputFolder.replace(/"/g, '');
         assert(DRIVE_LETTERS.includes(drive), 'Invalid drive');
+        const fromDir = `${drive}:\\\\demo-pics`; // @todo: determine camera output
+        const outStat = await fs.stat(outputFolder).catch(_ => null);
 
-        // @todo: implement real logic here
-        await delay(1200);
+        if (!outStat) {
+            await fs.mkdir(outputFolder);
+        }
 
-        return 'C:\\code\\plant-health-monitor\\img\\for-stitch';
+        const folderStats = await fs.stat(fromDir).catch(_ => null);
+
+        if (!folderStats || !folderStats.isDirectory()) {
+            return {
+                [EXEC_ERROR]: true,
+                stdout: '',
+                stderr: 'ENOSRCDIR'
+            };
+        }
+
+        const files = await getAllFiles(fromDir);
+
+        for (const file of files) {
+            const relPath = path.relative(fromDir, file)
+            const outputFile = path.resolve(outputFolder, relPath);
+            await fs.copyFile(file, outputFile)
+        }
+
+        return true;
     }
 }
