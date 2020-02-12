db7d4b83d497f590f5d4a2ab9d7c1c4cc6bf063a
Josh Costa <joshcos97@gmail.com>
2020-02-12 17:28:16 -0600

    Revert "make them degrees"
    This reverts commit 7a0f1617e5693800398d84aa88eb243a9ca82a07.

--- components/navigation/Onboard-SDK/sample/platform/linux/dphm-v0/mission_sample.cpp
+++ components/navigation/Onboard-SDK/sample/platform/linux/dphm-v0/mission_sample.cpp
@@ -83,7 +83,6 @@ runDPHMMission(Vehicle* vehicle, int responseTimeout)
   }
   else
   {
-    DSTATUS("Im flying weeeeeeeeeee\n");
     sleep(15);
   }
 
@@ -217,9 +216,9 @@ generateWaypointsFromFile(WayPointSettings* start_data)
     double altitude = std::stod(alt_str);
     double pi = 3.14159265358979323846;
 
-    //latitude = (latitude / 360) * 2 * pi;
-    //longitude = (longitude / 360) * 2 * pi;
-  
+    latitude = (latitude / 360) * 2 * pi;
+    longitude = (longitude / 360) * 2 * pi;
+    
     WayPointSettings wp;
     setWaypointDefaults(&wp);
     wp.index     = i;
